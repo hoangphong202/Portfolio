3d1cfb489d09e215373711258ad497a6dbc41962
hoangphong202 <hoangtrungphongit202@gmail.com>
2025-12-15 12:49:16 +0700

    UPDATE

--- src/components/Projects/IllustrationSection.jsx
+++ src/components/Projects/IllustrationSection.jsx
@@ -26,7 +26,7 @@ import bg6 from "../../assets/illustrations/background/6.png";
 import bg7 from "../../assets/illustrations/background/7.png";
 import bg8 from "../../assets/illustrations/background/8.png";
 import bg9 from "../../assets/illustrations/background/9.png";
-import bg10 from "../../assets/illustrations/background/10.png";
+import bg14 from "../../assets/illustrations/background/14.png";
 import bg11 from "../../assets/illustrations/background/11.jpg";
 import bg12 from "../../assets/illustrations/background/12.jpg";
 import bg13 from "../../assets/illustrations/background/13.png";
@@ -62,6 +62,7 @@ const illustrations = [
   { src: bg1, category: "Background" },
   { src: bg5, category: "Background" },
   { src: bg13, category: "Background" },
+  { src: bg14, category: "Background" },
   { src: bg11, category: "Background" },
   { src: bg2, category: "Background" },
   { src: bg12, category: "Background" },
